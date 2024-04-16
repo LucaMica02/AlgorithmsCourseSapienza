2fe99106764644a75771dad4ed484dfb8b7282ad
LucaMica02 <luca.micarelli02@gmail.com>
2024-04-16 22:38:49 +0200

    Update

--- Dividi_Et_Impera/Dividi_Et_Impera_Exercises.py
+++ Dividi_Et_Impera/Dividi_Et_Impera_Exercises.py
@@ -55,3 +55,78 @@ def findRank3(array, k):
     else:
         return findRank3(right, k - len(left) - 1)
 # Time complexity -> T(n) = T(n/5) + T(3/4(n)) + O(n) -> O(n)
+
+
+'''
+Given two integers a,n design an algorithm that compute a^n using only the +,*,// operators
+'''
+def pow1(a,n):
+    res = 1
+    for i in range(n):
+        res *= a
+    return res
+# Time complexity -> O(n)
+
+'''
+Idea: 
+if n is even -> a^n = a^n//2 * a^n//2           [2^10 = 2^5 * 2^5]
+if n is odd  -> a^n = a^n//2 * a^n//2 * a       [2^11 = 2^5 * 2^5 * 2]
+and so on..
+'''
+def pow2(a,n):
+    if n == 0:
+        return 1
+    x = pow2(a, n//2)
+    if n % 2 == 0:
+        return x*x
+    return x*x*a
+# Time complexity -> O(logn)
+
+
+'''
+Given a binary string S of n bits design an algorithm that calculates 
+the number of substrings of S starting with 0 and ending with 1
+'''
+def findSub1(S):
+    res, n = 0, len(S)
+    for i in range(n):
+        if S[i] == '0':
+            curr = '' + S[i]
+            for j in range(i + 1, n):
+                curr += S[j]
+                if curr[-1] == '1':
+                    res += 1
+    return res
+# Time complexity -> O(n^2)
+
+'''
+Idea: split the list into two, solve the subproblems and return left solve + right solve + curr
+where curr is the number of substrings that start at the left and end at the right
+'''
+def findSub2(S):
+    n = len(S)
+    if n <= 1:
+        return 0
+    n //= 2
+    left = findSub2(S[:n])
+    right = findSub2(S[n:])
+    curr = S[:n].count('0') * S[n:].count('1')
+    return left + right + curr
+# Time complexity -> T(n) = 2T(n/2) + O(n) =-> O(nlogn)
+
+'''
+managing the indexes and the returns i can do the operations in O(1) instead O(n)
+'''
+def findSub3(S):
+
+    def helper(S, i, j):
+        if i == j:
+            return [0, S[i].count('0'), S[i].count('1')]
+        n = (i + j) // 2
+        left = helper(S, i, n)
+        right = helper(S, n+1, j)
+        curr = left[1] * right[2]
+        return [left[0] + right[0] + curr, left[1] + right[1], left[2] + right[2]] 
+    
+    return helper(S, 0, len(S)-1)[0]
+# Time complexity -> T(n) = 2T(n/2) + O(1) =-> O(n)
